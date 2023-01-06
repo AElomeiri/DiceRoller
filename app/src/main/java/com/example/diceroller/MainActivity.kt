f0b3195726aabeab5b4b6ed9c91a4ed19d3705f4
AElomeiri <elomeiri.abdelrahman@gmail.com>
2023-01-06 19:24:09 +0200

    A button and a textview were added, also function (rollDice) to generate a random numder betweetn (1 - 6) when the user click the button, and it appears on the textview

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollButton.text = "Roll"
+        }
+        textResult = findViewById(R.id.text_result)
     }
+
+    private fun rollDice() {
+        textResult.text = (Random().nextInt(6) + 1).toString()
+    }
+
 }
